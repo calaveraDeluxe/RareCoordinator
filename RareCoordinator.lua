ddd74cc75029ffec5883906024a5759c6d18f5a3
elvador <>
2013-03-31 14:10:49 +0200

    Fixed show/hide function

--- RareCoordinator.lua
+++ RareCoordinator.lua
@@ -218,7 +218,7 @@ local needStatus = false
 
 --------------------------------
 local RC = CreateFrame("Frame", "RC", UIParent)
-RC.version = "5.2.0-8"
+RC.version = "5.2.0-9"
 
 
 function RC:getLocalRareName(id)
